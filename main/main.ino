b0a21469bf97b497e9b7cf653594a3797dc7f9cd
EricJeong <ejeong5@uw.edu>
2022-03-31 16:18:15 -0700

    Updated Arduino file

--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,18 @@
+
+#define enableMotorA 9
+#define input1 6
+#define input2 7
+
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(enableMotorA, OUTPUT);
+  pinMode(input1, OUTPUT);
+  pinMode(input2, OUTPUT);
 
+  // Set rotation direction
+  digitalWrite(input1, LOW);
+  digitalWrite(input2, HIGH);
 }
 
 void loop() {
